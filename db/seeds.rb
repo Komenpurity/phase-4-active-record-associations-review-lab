e4190c57e5f76ceea64bc72980dceb6943ca587b
Komenpurity <jepkorirpurity8@gmail.com>
2023-06-27 11:36:04 +0300

    complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Starting seeding!"
+
+Passenger.create(passenger_name: "Michael")
+Taxi.create(taxi_name: "Bolt Ke", car_plate: "KXE-234") 
+
+puts "Seeding complete!"
